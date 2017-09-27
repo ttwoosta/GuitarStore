740a530529d12be8fa910dbf5faf3439c3886bd2
Tu Tong <ttwoosta@gmail.com>
2017-09-27 08:16:43 -0400

    Implemented buttons event handlers.

--- GuitarStore/frmGuitarStore.vb
+++ GuitarStore/frmGuitarStore.vb
@@ -1,9 +1,58 @@
-﻿Public Class frmGuitarStore
+﻿' Program Name: Guitar Store
+' Developer:    Tu Tong
+' Date:         September 26, 2017
+' Purpose:      This application displays three guitar types
+'               (classical, steel-string, and electric).
+'               The user can select a guitar type and purchase it.
+
+Public Class frmGuitarStore
+
+    Private Sub rdoClassicalAcoustic_CheckedChanged(sender As Object, e As EventArgs) Handles rdoClassicalAcoustic.CheckedChanged
+        ' This code is executed when user taps or clicks 
+        ' the Classical acoustic radio button.
+        ' It displays the Classical guitar picture.
+
+        picGuitar.Image = My.Resources.classical
+
+    End Sub
+
+    Private Sub rdoSteelStringAcoustic_CheckedChanged(sender As Object, e As EventArgs) Handles rdoSteelStringAcoustic.CheckedChanged
+        ' This code is executed when user taps or clicks 
+        ' the Steel - String  Acoustic radio button.
+        ' It displays the Steel-string guitar picture.
+
+        picGuitar.Image = My.Resources.steel_string
+
+    End Sub
+
+    Private Sub rdoElectricGuitar_CheckedChanged(sender As Object, e As EventArgs) Handles rdoElectricGuitar.CheckedChanged
+        ' This code is executed when user taps or clicks
+        ' the Electric Guitar radio button.
+        ' It displays the Eletric guitar picture.
+
+        picGuitar.Image = My.Resources.eletric
+
+    End Sub
+
+    Private Sub btnPurchase_Click(sender As Object, e As EventArgs) Handles btnPurchase.Click
+        ' This code is executed when user taps or clicks
+        ' the Purchase button. It disables Purchase button,
+        ' disable Guitar Choice group,
+        ' displays the Enjoy label, and enables Exit button.
+
+        btnPurchase.Enabled = False
+        grpGuitarChoices.Enabled = False
+        lblEnjoy.Visible = True
+        btnExit.Enabled = True
+
+    End Sub
+
     Private Sub btnExit_Click(sender As Object, e As EventArgs) Handles btnExit.Click
+        ' This code is executed when users taps or clicks the Exit button.
+        ' It close the window and terminates the program execution.
+
+        Close()
 
     End Sub
 
-    Private Sub picGuitar_Click(sender As Object, e As EventArgs) Handles picGuitar.Click
-
-    End Sub
 End Class
